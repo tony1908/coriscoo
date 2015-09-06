e2826358848fbfd6c25b7e8f0bdf1fc256a478dc
Antonio <toony1908@gmail.com>
2015-09-05 21:03:11 -0500

    d

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,8 +5,55 @@ Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
-Rails.application.config.assets.precompile += %w( 0.jpg )
-Rails.application.config.assets.precompile += %w( landscaping button.jpg )
+# config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif]
+# Rails.application.config.assets.precompile += %w( 0.jpg )
+# Rails.application.config.assets.precompile += %w( landscaping button.jpg )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
