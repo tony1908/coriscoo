3c820fe7414bafa448ab19803c355f9b31b1df97
Antonio <toony1908@gmail.com>
2015-09-20 11:10:25 -0500

    d

--- app/uploaders/pdf_uploader.rb
+++ app/uploaders/pdf_uploader.rb
@@ -2,7 +2,7 @@
 
 class PdfUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+  # include CarrierWave::RMagick f
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
